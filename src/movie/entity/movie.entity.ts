3c8e5cbfde4f57c96526ee97a06dc11a02e13e93
Ryuji <hi_ryuji@naver.com>
2025-03-18 22:39:51 +0900

    feat: DataSource 정의하고 환경변수 사용해보기

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,11 +1,5 @@
-import { Exclude, Expose, Transform } from 'class-transformer';
-
 export class Movie {
   id: number;
   title: string;
-
-  @Transform(({ value }) => {
-    value.toString().toUpperCase();
-  })
   genre: string;
 }
