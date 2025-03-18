dfe2571be4ade07c5b7d51a6ee9443e5a81a8a28
Ryuji <hi_ryuji@naver.com>
2025-03-18 23:11:59 +0900

    feat: TypeORM으로 테이블 생성하기

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,5 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
+
+@Entity()
 export class Movie {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
+
+  @Column()
   genre: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
 }
