6953e88238653858d0143e37f6616345e6c1ec8e
Ryuji <hi_ryuji@naver.com>
2025-04-03 18:08:51 +0900

    feat: Public Custom Decorator 생성하기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,19 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './strategy/local.strategy';
 import { User } from 'src/user/entity/user.entity';
 import { JwtAuthGuard } from './strategy/jwt.strategy';
+import { Public } from './decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('register')
   registerUser(@Headers('authorization') token: string) {
     return this.authService.register(token);
   }
 
+  @Public()
   @Post('login')
   loginUser(@Headers('authorization') token: string) {
     return this.authService.login(token);
--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -1,9 +1,17 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { Public } from '../decorator/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(context: ExecutionContext): boolean {
+    // public decoration이 되어있으면 모든 로직을 bypass
+    const isPublic = this.reflector.get(Public, context.getHandler());
+    if (isPublic) return true;
+
     // 요청에서 user 객체가 존재하는지 확인
     const request = context
       .switchToHttp()
