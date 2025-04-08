807f8413eeecb9b971f3326fbfcf8e3aa6c28e4f
Ryuji <hi_ryuji@naver.com>
2025-04-08 21:17:31 +0900

    feat: Response Time Interceptor 구현하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,8 +20,9 @@ import { User } from './user/entity/user.entity';
 import { envVariableKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
 import { AuthGuard } from './auth/guard/auth.guard';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { RBACGuard } from './auth/guard/rbac.guard';
+import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 
 @Module({
   imports: [
@@ -62,6 +63,7 @@ import { RBACGuard } from './auth/guard/rbac.guard';
   providers: [
     { provide: APP_GUARD, useClass: AuthGuard },
     { provide: APP_GUARD, useClass: RBACGuard },
+    { provide: APP_INTERCEPTOR, useClass: ResponseTimeInterceptor },
   ],
 })
 export class AppModule implements NestModule {
---
+++ src/common/interceptor/response-time.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+  NestInterceptor,
+  Request,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class ResponseTimeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    const req = context.switchToHttp().getRequest<Request>();
+    const reqTime = Date.now();
+
+    return next.handle().pipe(
+      //   delay(1000),
+      tap(() => {
+        const resTime = Date.now();
+        const diff = resTime - reqTime;
+
+        if (diff > 1000) {
+          console.log(`TIMEOUT: [${req.method} ${req.url}] ${diff}ms`);
+          throw new InternalServerErrorException(
+            '서버 처리 시간이 너무 오래 걸렸습니다.',
+          );
+        } else console.log(`[${req.method} ${req.url}] ${diff}ms`);
+      }),
+    );
+  }
+}
