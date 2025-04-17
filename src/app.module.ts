c13fb9a0e514d04b69fbbd258e440f5e8b125da2
Ryuji <hi_ryuji@naver.com>
2025-04-17 17:17:18 +0900

    feat: 스태틱파일 서빙하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,8 @@ import { RBACGuard } from './auth/guard/rbac.guard';
 import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
 import { QueryFailedExceptionFilter } from './common/filter/query-failed.filter';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -55,6 +57,10 @@ import { QueryFailedExceptionFilter } from './common/filter/query-failed.filter'
         synchronize: true,
       }),
       inject: [ConfigService],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public'),
+      serveRoot: '/public',
     }),
     MovieModule,
     DirectorModule,
